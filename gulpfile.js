9cf646da3fe402b7c76063d688b5a024a21117d5
Paras Varshney <paras.varshney97@gmail.com>
2020-04-06 03:54:35 +0530

    testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,39 @@
-const gulp = require('gulp');
-const sass = require('./build/sass');
-const scripts = require('./build/scripts');
-const images = require('./build/images');
-const sync = require('./build/browsersync');
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const wait = require('gulp-wait');
+const babel = require('gulp-babel');;
+const rename = require('gulp-rename');
 
-[sass, scripts, images, sync].forEach(task => {
-  task(gulp);
+gulp.task('scripts', function() {
+    return gulp.src('./js/scripts.js')
+        .pipe(plumber(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        })))
+        .pipe(babel({
+          presets: [['@babel/env', {modules:false}]]
+        }))
+        .pipe(uglify({
+            output: {
+                comments: '/^!/'
+            }
+        }))
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('./js'));
 });
 
-gulp.task('build', ['sass', 'scripts', 'images', 'jekyll-build']);
+gulp.task('styles', function () {
+    return gulp.src('./scss/styles.scss')
+        .pipe(wait(250))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./js/scripts.js', gulp.series('scripts'));
+    gulp.watch('./scss/styles.scss', gulp.series('styles'));
+});
